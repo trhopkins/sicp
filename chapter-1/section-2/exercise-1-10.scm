53039bd3f423363ef51e2affac7392b313799133
Travis Reid Hopkins <trhopkins@protonmail.com>
2021-03-15 13:35:22 -0400

    fixed 1.10 and added helper functions

--- chapter-1/section-2/exercise-1-10.scm
+++ chapter-1/section-2/exercise-1-10.scm
@@ -20,3 +20,23 @@
 
 (A 3 3)
 ;; 65536
+
+;; Consider the following procedures, where A is the procedure defined above:
+
+(define (f n) (A 0 n))
+
+(define (g n) (A 1 n))
+
+(define (h n) (A 2 n))
+
+(define (k n) (* 5 n n))
+
+;; Give concise mathematical definitions for the functions computed by the
+;; procedures f, g, and h for positive integer values of n. For example, (k n)
+;; computes 5n^2.
+
+;; f = 2n
+
+;; g = 2^n
+
+;; h = 2^(2^n) or n Knuth arrows
