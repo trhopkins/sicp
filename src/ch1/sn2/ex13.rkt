509cc1f97f5f45b9d165d457c63623040a49b1b1
Travis Hopkins <trhopkins@protonmail.com>
2022-03-16 13:44:21 -0400

    Fixed fibonacci calculation test
    Replaced my old, janky code with a Rackunit test for the same thing.

--- src/ch1/sn2/ex13.rkt
+++ src/ch1/sn2/ex13.rkt
@@ -14,28 +14,11 @@
   (define (fib-iter a b count)
     (if (= count 1)
         b
-        (fib-iter b (+ a b) (- count 1))))
+        (fib-iter b (+ a b) (dec count))))
   (fib-iter 0 1 n))
 
-(define psi (/ (+ 1 (sqrt 5)) 2))
-(define phi (/ (- 1 (sqrt 5)) 2)) ; hint, may not be needed
-
-;; get number closest to psi^n/sqrt(5), compare to Fib(n)
-(define (closest? p f)
-  (if (<= (abs (- p f)) .5)
-      #t
-      #f))
-
-(define (power a b)
-  (define (power-iter a total count)
-    (if (= count 0)
-        total
-        (power-iter a (* a total) (- count 1))))
-  (power-iter a 1 b))
-
-
-;; lexically scoped square root by Newton's method
-(define (sqrt x) ; consider lambdas here later?
+;; lexically scoped square root by Newton's method, used for psi/phi
+(define (sqrt x)
   (define (sqrt-iter guess)
     (define (improve)
       (define (average a b)
@@ -48,26 +31,27 @@
         (sqrt-iter (improve))))
   (sqrt-iter 1.0))
 
-;; proof for some n
-(define (compare-all n)
-  (if (closest? (/ (power psi n) (sqrt 5)) (fib n))
-      (display "yes")
-      (display "no"))
-  (if (= n 0)
-      #f
-      (compare-all (- n 1))))
+(define psi (/ (+ 1 (sqrt 5)) 2))
+(define phi (/ (- 1 (sqrt 5)) 2)) ; hint, may not be needed
 
-;(compare-all 5)
+;; get number closest to psi^n/sqrt(5), compare to Fib(n)
+#;(define (closest? psi fib)
+  (<= (abs (- psi fib)) .5))
 
-(if (closest? (/ (power psi 10) (sqrt 5)) (fib 10))
-    (display "yes\n")
-    (display "no"))
+#;(define (check-all n)
+  (if (zero? n)
+      'done
+      (begin
+        (check-true (closest? (/ (expt psi 10) (sqrt 5)) (fib 10)))
+        (check-all (dec n)))))
 
-(define (asdf n)
-  (cond ((= n 0) (display "end"))
-        ((not (= n 0)) (display "not done\n")
-                       (asdf (- n 1)))))
-(asdf 3)
+#;(check-all 10)
 
-;; NOT DONE, CLEAN UP CODE
+(let* ((n 10)
+       #;(req (/ (expt psi n) (sqrt 5))) ; initial test
+       (req (/ (- (expt psi n)
+                  (expt phi n))
+               (sqrt 5))) ; sci-fi test
+       (res (fib n)))
+  (check-= req res 0.001))
 
